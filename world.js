336a0a5091133ac56a69014fcb59f6f5c4d31943
Nathan Downer <nathan.downer@mymona.uwi.edu>
2018-11-16 18:27:14 +0000

    completed exercise 3

--- world.js
+++ world.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-    const input = document.querySelector('input#text');
-    const lookup = document.querySelector('button');
+    const input = document.querySelector('#country');
+    const lookup = document.querySelector('#lookup');
     const display = document.querySelector('#result');
     
     lookup.addEventListener('click', getCountryInfo);
@@ -9,12 +9,15 @@ window.onload = function() {
     function getCountryInfo() {
         const http = new XMLHttpRequest();
         http.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200 ) {
-                display.innerHTML = this.responseText;
-            } else {
-                alert('Something went wrong with the request');
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    display.innerHTML = this.responseText;
+                } else {
+                    alert("There was an error.");
+                }
             }
-        }
+        };
+        console.log(input.value);
         http.open('GET',`./world.php?country=${input.value}`, true);
         http.send();
     }
--- world.php
+++ world.php
@@ -5,11 +5,29 @@ $username = getenv('C9_USER');
 $password = '';
 $dbname = 'world';
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+$country = $_GET['country'];
 
-$stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (Exception $e) {
+    die("Oh no! There's an error in the query!");
+}
+
+
+
+// $country = $_GET['country'];
+// if ($country = "") {
+//     $stmt = $conn->query("SELECT * FROM countries");
+// } else {
+//     $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE %$country%");
+// }
+
+// $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE %$country%");
+
+// $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 echo '<ul>';
 foreach ($results as $row) {
